f0062d87caa66c68ee706451faa7686ae175790e
= <=>
2024-10-12 17:08:35 +0700

    chessModel

---
+++ tst/chess/ChessModelTest.java
@@ -0,0 +1,15 @@
+package chess;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChessModelTest {
+
+	@Test
+	void testToString() {
+		ChessModel chessModel = new ChessModel();
+		System.out.println(chessModel);
+	}
+
+}
